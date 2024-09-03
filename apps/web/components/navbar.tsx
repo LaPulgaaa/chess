8ab7ef525b1125dbbc17f0e0781e59fd968eaa37
Varun Singh <varunsingh9114@gmail.com>
2024-09-03 23:29:33 +0530

    navbar: Add navbar component.

--- apps/web/components/navbar.tsx
+++ apps/web/components/navbar.tsx
@@ -1,20 +1,28 @@
 import Link from "next/link";
 
-import { ModeToggle } from "@repo/ui"
+import { ModeToggle, Button } from "@repo/ui";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export default function Navbar(){
     return (
         <header className="sticky top-0 z-full w-full backdrop-blur">
             <div className="container flex justify-between h-14 max-w-screen-2xl items-center">
-                <div className="mr-4 hidden md:flex">
+                <div className="mr-4 flex">
                     <Link 
                     className="mr-4 flex items-center space-x-2 lg:mr-6"
                     href="/">
-                        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">play/Chess</h4>
+                        <span className="hidden lg:inline-block font-bold">play/Chess</span>
                     </Link>
                 </div>
-                <div className="flex ">
+                <div className="flex">
                     <ModeToggle/>
+                    <a
+                    className="mx-2"
+                    href="https://github.com/LaPulgaaa/chess">
+                        <Button variant={"ghost"} size={"icon"}>
+                            <GitHubLogoIcon/>
+                        </Button>
+                    </a>
                 </div>
             </div>
         </header>
