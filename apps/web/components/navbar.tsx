37675d9b2acf6a7d47b219f63bc68f4c7454f9bf
Varun Singh <varunsingh9114@gmail.com>
2024-09-04 01:15:27 +0530

    navbar: Add top margin & change header text to be italic.

--- apps/web/components/navbar.tsx
+++ apps/web/components/navbar.tsx
@@ -6,12 +6,12 @@ import { GitHubLogoIcon } from "@radix-ui/react-icons";
 export default function Navbar(){
     return (
         <header className="sticky top-0 z-full w-full backdrop-blur">
-            <div className="container flex justify-between h-14 max-w-screen-2xl items-center">
+            <div className="container flex justify-between h-14 max-w-screen-2xl items-center mt-1">
                 <div className="mr-4 flex">
                     <Link 
                     className="mr-4 flex items-center space-x-2 lg:mr-6"
                     href="/">
-                        <span className="hidden lg:inline-block font-bold">play/Chess</span>
+                        <span className="hidden italic lg:inline-block font-bold">play/ Chess</span>
                     </Link>
                 </div>
                 <div className="flex">
