9e338ff7b386bb370d20492296a3998273e53853
Varun Singh <varunsingh9114@gmail.com>
2024-09-29 12:14:33 +0530

    game: Add selector to fetch a live game with game_id.

--- packages/store/atom/game.ts
+++ packages/store/atom/game.ts
@@ -2,15 +2,15 @@ import { atomFamily, selectorFamily } from "recoil";
 
 import { LiveGameState } from "@repo/types";
 
-export const live_games_store = atomFamily<LiveGameState[] | null, {user_id: string}>({
+export const live_games_store = atomFamily<LiveGameState[] | null, {username: string}>({
     key: "games_store",
     default: selectorFamily({
         key: "get_games",
         get: 
-        ({ user_id }:{ user_id: string }) => 
+        ({ username }:{ username: string }) => 
             async ({get}) => {
                 try{
-                    const resp = await fetch(`/api/player/${user_id}`,{
+                    const resp = await fetch(`/api/player/${username}`,{
                         cache: "no-store"
                     });
                     const {data}:{data:LiveGameState[]} = await resp.json();
@@ -22,3 +22,18 @@ export const live_games_store = atomFamily<LiveGameState[] | null, {user_id: str
             }
     })
 });
+
+export const get_game_from_id = selectorFamily<LiveGameState | null, {username: string, game_id: string}>({
+    key: "get_game_from_id",
+    get: ({username, game_id}:{username: string, game_id: string}) =>
+        ({get}) => {
+            const all_live_games = get(live_games_store({username}));
+
+            const possible_live_game = all_live_games?.find((game) => game.game_id === game_id);
+
+            if(possible_live_game !== undefined)
+                return possible_live_game;
+
+            return null;
+        }
+})
