802eed81a8f664b1c86570bd46682e65fd0a5eb3
Varun Singh <varunsingh9114@gmail.com>
2024-10-12 01:00:06 +0530

    random: Replace Board component with DummyBoard component.

--- apps/web/app/home/play/online/random/page.tsx
+++ apps/web/app/home/play/online/random/page.tsx
@@ -1,10 +1,10 @@
-import Board from "../board";
+import DummyBoard from "../dummy_board";
 
 export default function Online(){
     const fen = "rnbqkbnr/pppp1ppp/8/4p3/4PP2/8/PPPP2PP/RNBQKBNR b KQkq - 0 2";
     return(
         <div  className="flex lg:flex-row flex-col items-center lg:justify-around mt-12">
-            <Board fen={fen} />
+            <DummyBoard/>
             <div>Display moves</div>
         </div>
     )
