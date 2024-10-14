cafce907deec0136f15f8a9b1bd08f1c75ab6f76
Varun Singh <varunsingh9114@gmail.com>
2024-10-14 19:36:20 +0530

    game: Add moves table.

--- apps/web/app/home/play/online/game/[game_id]/page.tsx
+++ apps/web/app/home/play/online/game/[game_id]/page.tsx
@@ -3,14 +3,15 @@
 import { FlipVertical } from "lucide-react";
 
 import { get_game_from_id } from "@repo/store";
-import { Button } from "@repo/ui";
+import { Button, ScrollArea } from "@repo/ui";
 import Board from "../../board";
 import type { Square, PieceSymbol, Color} from "chess.js"
 import { useSession } from "next-auth/react";
 import { useRecoilValueLoadable } from "recoil";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { SignallingManager } from "@/lib/singleton/signal_manager";
 import { GameManager } from "@/lib/singleton/game_manager";
+import { StarFilledIcon } from "@radix-ui/react-icons";
 
 type MoveCallbackData = {
     from: string,
@@ -30,9 +31,12 @@ export default function Game({params}:{params: {game_id: string}}){
     const game_state = useRecoilValueLoadable(get_game_from_id({game_id}));
     const [board,setBoard] = useState<Board | undefined>(undefined);
     const [orient,setOrient] = useState<"w" | "b">("w");
+    const [moves,setMoves] = useState<string[]>([]);
+    const moves_ref = useRef<HTMLDivElement>(null);
 
     function move_callback(raw_data:string){
         const data:MoveCallbackData = JSON.parse(raw_data);
+        setMoves((moves) => [...moves,data.to]);
         let updated_state = GameManager.get_instance().make_move(game_id,data.from,data.to);
         setBoard(updated_state)
     }
@@ -70,6 +74,7 @@ export default function Game({params}:{params: {game_id: string}}){
             const board = GameManager.get_instance().add_game(game_id, game_state.getValue()!.fen);
             setBoard(board);
             setOrient(game_state.getValue()!.color);
+            setMoves(game_state.getValue()!.plays);
         }
     },[game_state.state, game_state.getValue()]);
 
@@ -110,7 +115,32 @@ export default function Game({params}:{params: {game_id: string}}){
                             }
                         </div>
                     </div>
-                    <div className="w-full">Record moves her</div>
+                    <div className="w-[720px] mx-4">
+                        <div className="flex items-center dark:bg-zinc-800 p-2 py-3">
+                            <StarFilledIcon className="mx-2"/>
+                            <h3 className="scroll-m-20 font-semibold tracking-tight">
+                                Moves Played
+                            </h3>
+                        </div>
+                        <ScrollArea className=" h-[600px]">
+                            <div 
+                            ref={moves_ref}
+                            className="grid grid-cols-2">
+                            {
+                                moves.map((move,i) => {
+                                    return(
+                                        <div
+                                        key={i}
+                                        id="moves"
+                                        className={`p-2 my-1 ${i%2 == 0 ? "dark:bg-zinc-700" : "dark:bg-zinc-800"}`}>
+                                            {move}
+                                        </div>
+                                    )
+                                })
+                            }
+                            </div>
+                        </ScrollArea>
+                    </div>
                 </div> : 
                 <div>Loading...</div>
             }
