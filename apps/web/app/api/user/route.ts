3fd1273356b0a02de28e2f89b483c8b46011db2c
Varun Singh <varunsingh9114@gmail.com>
2024-11-06 17:49:49 +0530

    user: Fix incorrect placement of return statement.

--- apps/web/app/api/user/route.ts
+++ apps/web/app/api/user/route.ts
@@ -5,25 +5,26 @@ export async function POST(req: Request){
     const raw_data = await req.json();
     try{
         const data = user_signin_form_schema.parse(raw_data);
-        const user = await prisma.$transaction(async(tx)=>{
+        const username = await prisma.$transaction(async(tx)=>{
             const new_user = await tx.user.create({
                 data:{
                     username: data.email,
                     email: data.email
                 }
             });
-            const user_password = await tx.password.create({
+            await tx.password.create({
                 data: {
                     hash: data.password,
                     user_id: new_user.id
                 }
             });
-    
-            return Response.json({
-                msg: "success",
-                data: new_user.username
-            })
+
+            return new_user.username
         })
+        return Response.json({
+            msg: "success",
+            data: username
+        },{ status: 201 });
     }catch(err){
         return new Response('Could not signup',{
             status: 400,
